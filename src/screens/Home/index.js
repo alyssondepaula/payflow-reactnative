1b5a38eedf413c6541631d64bec45eee25c9ff82
alyssondepaula <alyssonjuniordepaula@gmail.com>
2021-08-18 19:55:52 -0300

    feat: Add Home Screen file

---
+++ src/screens/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { Container } from './styles';
+
+const Home = () => {
+  return <Container>
+     <StatusBar backgroundColor="orange" barStyle={'light-content'} />
+    
+   
+  </Container>
+}
+
+export default Home;
---
+++ src/screens/Home/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+import person from '../../../assets/images/person.png'
+import logomini from '../../../assets/images/logomini.png'
+import { LinearGradient } from 'expo-linear-gradient';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: white;
+  flex-direction: column;
+`;
