ca58e82c8779f78f7e30034cfc99bf19daa106b0
alyssondepaula <alyssonjuniordepaula@gmail.com>
2021-08-18 19:48:55 -0300

    feat: config auth Routes

--- App.js
+++ App.js
@@ -1,22 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+
+import AuthProvider from './src/context/auth';
 import Routes from './src/Routes';
 
 export default function App() {
 
-
-
   return (
-    <Routes/>
+    <NavigationContainer>
+         <AuthProvider>
+           <Routes/>
+         </AuthProvider>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/Routes/AuthRoutes.js
+++ src/Routes/AuthRoutes.js
@@ -2,10 +2,33 @@ import React from 'react';
 import { View } from 'react-native';
 import SignUp from '../screens/SignUp';
 
-// import { Container } from './styles';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const config = {
+  animation: 'spring',
+  config: {
+    stiffness: 1000,
+    damping: 500,
+    mass: 3,
+    overshootClamping: true,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+  },
+};
 
 const AuthRoutes = () => {
-  return <SignUp />;
+  return   (
+  <Stack.Navigator 
+  initialRouteName="signup" 
+  screenOptions={{headerShown: false}}
+  >
+  <Stack.Screen
+  name="signup"
+  component={SignUp} 
+  />
+</Stack.Navigator>);
 }
 
 export default AuthRoutes;
